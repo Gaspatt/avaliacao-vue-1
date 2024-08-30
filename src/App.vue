90dd3e9cd15662c32168b02ee3cb76576418a2c9
gaspatt <matheusgaspar29.if@gmail.com>
2024-08-30 11:57:27 -0300

    .

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import ListarTarefas from './components/ListarTarefas.vue';
+import MainComp from './components/MainComp.vue';
+</script>
+
+<template>
+  <main-comp />
+  <listar-tarefas/>
+</template>
+
+<style scoped></style>
---
+++ src/components/ListarTarefas.vue
@@ -0,0 +1,44 @@
+<script setup>
+defineProps({
+  realizadas: {
+    type: Boolean,
+    default: false
+  },
+  tarefas: [
+    { id: 1, descricao: 'Fazer avaliação 1', feita: true },
+    { id: 2, descricao: 'Fazer avaliação 2', feita: false },
+    { id: 3, descricao: 'Fazer avaliação 3', feita: false },
+    { id: 4, descricao: 'Fazer avaliação 4', feita: false },
+    { id: 5, descricao: 'Fazer avaliação 5', feita: false }
+  ]
+})
+</script>
+
+<template>
+  <section
+    v-for="tarefa in tarefas"
+    :key="tarefa.id"
+    :class="{ feita: tarefa.feita }"
+    @click="$emit('click', tarefa.id)"
+  >
+    <Transition name="fade">
+      <p v-if="realizadas ? tarefa.feita : !tarefa.feita">{{ tarefa.descricao }}</p>
+    </Transition>
+  </section>
+</template>
+
+<style scoped>
+.feita {
+  text-decoration: line-through;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 2s ease-in-out;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
